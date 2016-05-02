92eaebbb27fd3122f93ae23c6b48ee3d19f0b8cd
Marcos Pereira <marcos.pereira@itec.al.gov.br>
2016-05-02 10:15:50 -0300

    Create project django and app.

--- README.md
+++ README.md
@@ -3,9 +3,47 @@ DemoApp - Friday of Knowledge
 
 Demo application for the event "Friday of Knowledge".
 
+
+## Initiation ##
+
+* Create project:
+
+```
+django-admin.py startproject coffeemanager
+```
+
+* Create app:
+
+```
+python manage.py startapp coffe
+```
+
+* Create models and admin:
+
+```
+See coffee package in project.
+```
+
+* Install requirements:
+
+```
+See section Requirements.
+```
+
+* Create API Rest (purpose of the event):
+
+```
+See the presentation of the event [1].
+```
+
 ## Requirements ##
 
-* [npm](https://www.npmjs.com/)
-* [bower](http://bower.io/)
-* [AngularJS](https://angularjs.org/)
-* [Bootstrap](http://getbootstrap.com/)
+* django 1.8 (https://www.djangoproject.com/)
+* djangorestframework (http://www.django-rest-framework.org/)
+* markdown (http://pythonhosted.org/Markdown/)
+* django-filter (https://github.com/carltongibson/django-filter)
+
+## References ##
+
+* [1] Link to apresentation
+* [2] Link to app
---
+++ coffee_manager/settings.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+    Demo application for the event
+    Friday of Knowledge
+
+    CoffeeManager
+    @author: Marcos Pereira
+"""
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'z1%n^ag0*e6aez5r&=9p59$cal7^3a&g=f#%k3s_dgco4kz@iy'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+# Application definition
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+ROOT_URLCONF = 'coffee_manager.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'coffee_manager.wsgi.application'
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
