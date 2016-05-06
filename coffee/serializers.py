91eb3d8ed9d91ea8f13f4436f07ce4318b963399
Marcos Pereira <marcospereira@ideenbox.com.br>
2016-05-06 20:54:02 -0300

    Change return of QueueView. Change get method of the member_name.

--- coffee/serializers.py
+++ coffee/serializers.py
@@ -5,20 +5,20 @@ from coffee.models import Member, Registry
 
 
 class MemberSerializer(serializers.ModelSerializer):
+    '''
+        Modelo de usuários
+    '''
     registries = serializers.PrimaryKeyRelatedField(many=True, queryset=Registry.objects.all(), required=False)
 
     class Meta:
         model = Member
         fields = ('name', 'email', 'registries')
 
-
-class MemberRelatedField(serializers.RelatedField):
-    def to_representation(self, value):
-        return "%s" % (value.name)
-
-
 class RegistrySerializer(serializers.ModelSerializer):
-    member_name = MemberRelatedField(source='member', read_only=True)
+    member_name = serializers.SerializerMethodField(source="get_member_name")
+
+    def get_member_name(self, obj):
+        return obj.member.name
 
     class Meta:
         model = Registry
